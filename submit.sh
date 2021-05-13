02abbb2adddbbc8190fd663d6068f8eb3d771ffc
MartonLeitold <martonleitold@flexecharge.com>
2021-05-13 15:45:40 +0200

    Adding folder for submit and the bsub code

---
+++ submit.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+#BSUB -J gaussSeidel
+
+#BSUB -n 1
+#BSUB -R "span[ptile=24]"
+#BSUB -W 00:59
+#BSUB -q hpcintro
+#BSUB -R "rusage[mem=8GB]"
+#BSUB -oo Output.out
+#BSUB -eo Error.err
+
+module purge
+module load studio
+module load mpi/3.1.3-gcc-8.2.0
+
+mpicc -o main -O2 -ftree-vectorize -march=native main.c
+
+mpirun ./main 1000
